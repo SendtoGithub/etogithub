b1af67c47f1d855fb6a0cb761f6e56275a6fab3a
SendtoGithub <niharika8500@gmail.com>
2022-01-28 14:44:11 +0530

    Three commit....

---
+++ src/eegpg/Teest.java
@@ -0,0 +1,16 @@
+package eegpg;
+
+public class Teest {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes") {
+			System.out.println("eat something.....");
+		}
+		else {
+			System.out.println("do work......");
+		}
+
+	}
+
+}
